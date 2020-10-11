1862956c91a82a7866f8de943336f8d97fdb29d9
Mihir Thanekar <mihirthanekar@gmail.com>
2020-10-11 16:00:32 -0700

    Made super cool python file

---
+++ cool.py
@@ -0,0 +1,8 @@
+# absolutely no documentation
+def cool():
+    while(True):
+        while(True):
+            while(True):
+                while(True):
+                    while(True):
+                        pass
